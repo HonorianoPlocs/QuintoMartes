e1b9f814826f92252f58916f6e019851493e64b5
Eale3563 <eale80404@gmail.com>
2025-04-15 13:10:30 -0500

    ultimocomit

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ const app=express()
 const useRouter=require('./routers/userRouters')
 const morgan=require('morgan')
 const userLogin=require('./middlewares/userLogin')
+const path=require('path')
 
 app.use(express.json())
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','ejs')
+
 app.get('/',(req,res)=>{
-    console.log('Servidor creado con express.js')
-    res.send("Accediendo a usaurio con wuazaID:"+req.params.id)
+    const data={
+        "title":"Titulo de la pagina",
+        "message":"Bienvenido a mi sitio web",
+        "showMessage":true,
+        "items":[1,2,3,4,5]
+    }
+
+    res.render('index',data)
 })
 
 app.use('/users',useRouter)
