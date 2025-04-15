e8881838d9b3c828d1b5eb0572ddc1c366780dc1
Eale3563 <eale80404@gmail.com>
2025-04-15 08:52:16 -0500

    ti

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-const http = require('http')
-const server = http.createServer((request, Response) => {
-    if(request.url=='/'&& request.method=='GET')
-    Response.end(JSON.stringify({"message":"Accediendo a la cupula"}))
-})
 
-const port = 5000
-const host = "localhost"
-
-server.listen(port, host, () => {
-    console.log(`Servidor ejecutandose en: http://${port}:${host}`)
-})
