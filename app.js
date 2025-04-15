d2370e8fc615e0c94635ba88539f846bd9c87330
Eale3563 <eale80404@gmail.com>
2025-04-15 11:38:56 -0500

    ultimocomit

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+const express=require('express')
+const app=express()
+const useRouter=require('./routers/userRouters')
+const morgan=require('morgan')
+const userLogin=require('./middlewares/userLogin')
 
+app.use(express.json())
+app.get('/',(req,res)=>{
+    console.log('Servidor creado con express.js')
+    res.send("Accediendo a usaurio con wuazaID:"+req.params.id)
+})
+
+app.use('/users',useRouter)
+app.use(morgan('dev'))
+app.use(userLogin)
+
+
+
+app.listen(3000,()=>{
+    console.log('Aplicacion con express ejecutandose en el puerto 3000')
+})
